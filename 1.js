e9b8d0ef4965a391d01989435da23da398926f9f
= <=>
2020-01-29 09:41:11 +0700

    co ban xong file 1.js

---
+++ 1.js
@@ -0,0 +1,21 @@
+﻿ $(function(){
+ 	//click vao nut .bonnut li nao thi nut do thuc hien len
+
+ 	$('.bonnut li').click(function(){
+ 		// Ban dau no xoa het cac class la active
+ 		$('.bonnut li').removeClass('active');
+ 		// roi them class active vao de nhan cai nao thi cac do hien ra
+ 		$(this).addClass('active');
+
+ 		// Hien vi tri cua phan tu khi nhan vao roi gan cho x
+ 		x=$(this).index();
+ 		// Vi trong mang thi phan tu dau tien co gia tri 0
+ 		x=x+1;
+ 		// Ban dau no cung xoa het cac classs hienlen khi bi click
+ 		$(".bonnd ul li").removeClass("hienlen");
+ 		// .bonnd ul li:nth-child("+x+") la chi dinh vi tri x de them class vao li
+ 		// vi trong nth-child(number) nen muon them x ta phai noi ("+x+")
+ 		$(".bonnd ul li:nth-child("+x+") ").addClass("hienlen");
+ 	})
+ }) 
+ 
